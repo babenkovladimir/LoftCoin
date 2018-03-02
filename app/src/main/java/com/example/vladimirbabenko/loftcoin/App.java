0bfc465c2d3b018395aa4aa67a3bbd8e77c0491a
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-03-02 20:26:42 +0200

    add retrofit 2; add Gson converter;

---
+++ app/src/main/java/com/example/vladimirbabenko/loftcoin/App.java
@@ -0,0 +1,14 @@
+package com.example.vladimirbabenko.loftcoin;
+
+import android.app.Application;
+
+/*
+* Этот класс предназначен для инициалтзации наших библиотек.
+* */
+
+public class App extends Application {
+
+  @Override public void onCreate() {
+    super.onCreate();
+  }
+}
