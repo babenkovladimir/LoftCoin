de9aea0dfdbac1318c9a1e5ec219335d01d9a1fa
Babenko Vladimir <babenkovladimirbmd@gmail.com>
2018-03-04 10:46:05 +0200

    final commit;

--- app/src/main/java/com/example/vladimirbabenko/loftcoin/CoinsAdapter.java
+++ app/src/main/java/com/example/vladimirbabenko/loftcoin/CoinsAdapter.java
@@ -1,6 +1,8 @@
 package com.example.vladimirbabenko.loftcoin;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,62 +11,72 @@ import android.widget.TextView;
 
 import java.util.Collections;
 import java.util.List;
-
+import java.util.Random;
 
 public class CoinsAdapter extends RecyclerView.Adapter<CoinsAdapter.CoinsViewHolder> {
 
-    private List<Coin> coins = Collections.EMPTY_LIST;
+  private List<Coin> coins = Collections.EMPTY_LIST;
 
-    // Сщздаёт холдер
-    @Override
-    public CoinsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = null;
+  // Сщздаёт холдер
+  @Override public CoinsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = null;
+    //        LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    //        view = inflater.inflate(R.layout.item_coin, parent);
+    view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_coin, parent, false);
+    return new CoinsViewHolder(view);
+  }
 
-//        LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        view = inflater.inflate(R.layout.item_coin, parent);
+  // Наполняет ViewHolder данными
+  @Override public void onBindViewHolder(CoinsViewHolder holder, int position) {
+    holder.bind(coins.get(position));
+  }
 
-        view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_coin, parent, false);
+  // Возвращаем количество монет.
+  @Override public int getItemCount() {
+    return coins.size();
+  }
 
-        return new CoinsViewHolder(view);
+  public void setCoins(List<Coin> coins) {
+    this.coins = coins;
+    notifyDataSetChanged();
+  }
+  // Внутренний статический клас, которыйм наполняется
+
+  static class CoinsViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView symbol;
+    private TextView name;
+    private TextView price;
+    private TextView percentChange;
+    private Random random = new Random();
+    private static int[] colors =
+        {0xFF9C27B0, 0xFF673Ab7, 0xFF2196F3, 0xFF009688, 0xFFFFC107, 0xFFFF9800 };
+    private Context context;
+
+    public CoinsViewHolder(View itemView) {
+      super(itemView);
+      context = itemView.getContext();
+      symbol = (TextView) itemView.findViewById(R.id.tvSymbol);
+      name = (TextView) itemView.findViewById(R.id.tvName);
+      price = (TextView) itemView.findViewById(R.id.tvPrice);
+      percentChange = (TextView) itemView.findViewById(R.id.tvChange);
     }
 
-    // Наполняет ViewHolder данными
-    @Override
-    public void onBindViewHolder(CoinsViewHolder holder, int position) {
-        holder.bind(coins.get(position));
+    public void bind(Coin coin) {
+      symbol.setText(coin.symbol);
+      Drawable background = symbol.getBackground();
+      Drawable wrapper = DrawableCompat.wrap(background);
+      DrawableCompat.setTint(wrapper, colors[random.nextInt(colors.length)]);
+
+      name.setText(coin.name);
+
+      price.setText(context.getString(R.string.price, coin.priceUsd));
+      percentChange.setText(context.getString(R.string.percent_change, coin.percentChange));
+
+      if (coin.percentChange >= 0)
+        percentChange.setTextColor(context.getResources().getColor(R.color.percentChangePositive));
+       else
+        percentChange.setTextColor(context.getResources().getColor(R.color.percentChangeNegative));
     }
-
-
-    // Возвращаем количество монет.
-    @Override
-    public int getItemCount() {
-        return coins.size();
-    }
-
-    public void setCoins(List<Coin> coins) {
-        this.coins = coins;
-        notifyDataSetChanged();
-    }
-
-    static class CoinsViewHolder extends RecyclerView.ViewHolder {
-
-        private TextView symbol;
-        private TextView name;
-        private TextView price;
-        private TextView percentChange;
-
-
-        public CoinsViewHolder(View itemView) {
-            super(itemView);
-
-            symbol = (TextView)itemView.findViewById(R.id.tvSymbol);
-            name = (TextView)itemView.findViewById(R.id.tvName);
-            price = (TextView)itemView.findViewById(R.id.tvPrice);
-            percentChange = (TextView)itemView.findViewById(R.id.tvChange);
-        }
-
-        public void bind(Coin coin){
-
-        }
-    }
+  }
 }
--- app/src/main/java/com/example/vladimirbabenko/loftcoin/MainActivity.java
+++ app/src/main/java/com/example/vladimirbabenko/loftcoin/MainActivity.java
@@ -1,48 +1,56 @@
 package com.example.vladimirbabenko.loftcoin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String TAG = this.getClass().getSimpleName();
-    private RecyclerView recycler;
+  private String TAG = this.getClass().getSimpleName();
+  private RecyclerView recycler;
+  private CoinsAdapter adapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    adapter = new CoinsAdapter();
 
-        recycler = (RecyclerView) findViewById(R.id.rvRecycler);
-        recycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    recycler = (RecyclerView) findViewById(R.id.rvRecycler);
+    recycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    recycler.setAdapter(adapter);
 
+    loadData();
+  }
 
-        loadData();
-    }
+  private void loadData() {
+    Api api = ((App) getApplication()).getApi();
 
-    private void loadData() {
-        Api api = ((App) getApplication()).getApi();
-        api.ticker().enqueue(new Callback<List<Coin>>() {
-            @Override
-            public void onResponse(Call<List<Coin>> call, Response<List<Coin>> response) {
-                Log.d(TAG, "onResponse: ");
-                List<Coin> coins = response.body();
-            }
+    api.ticker().enqueue(new Callback<List<Coin>>() {
+      @Override public void onResponse(Call<List<Coin>> call, Response<List<Coin>> response) {
+        Log.d(TAG, "onResponse: ");
+        List<Coin> coins = response.body();
 
-            @Override
-            public void onFailure(Call<List<Coin>> call, Throwable t) {
-                Log.d(TAG, "onFailure: ");
-                Log.d(TAG, "onFailure: ", t);
-            }
+        Collections.sort(coins, new Comparator<Coin>() {
+          @Override public int compare(Coin o1, Coin o2) {
+            return Double.compare(o2.priceUsd, o1.priceUsd);
+          }
         });
-    }
+
+        adapter.setCoins(coins);
+      }
+
+      @Override public void onFailure(Call<List<Coin>> call, Throwable t) {
+        Log.d(TAG, "onFailure: ");
+        Log.d(TAG, "onFailure: ", t);
+      }
+    });
+  }
 }
